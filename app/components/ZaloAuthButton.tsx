7b0930a2517d0524439a9f724e0627c86c9411a1
ducanhle31 <117511428+ducanhle31@users.noreply.github.com>
2024-09-22 15:02:12 +0700

    up

--- app/components/ZaloAuthButton.tsx
+++ app/components/ZaloAuthButton.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 export const ZaloAuthButton = () => {
   const handleLogin = () => {
-    window.location.href = "http://localhost:3001/api/v1/authzalo/login"; // Your backend URL
+    window.location.href = "http://10.10.51.16:3001/api/v1/authzalo/login"; // Your backend URL
   };
   return (
     <div className="zalo-login">
-      <button onClick={handleLogin} className="login-btn bg-blue-600 p-4 rounded-sm text-white hover:bg-blue-700">
+      <button
+        onClick={handleLogin}
+        className="login-btn bg-blue-600 p-4 rounded-sm text-white hover:bg-blue-700"
+      >
         Login with Zalo
       </button>
     </div>
